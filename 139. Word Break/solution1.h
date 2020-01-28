d7c7c950744d33f1fad4e2375387dd95eb84a260
linerxliner <linerxliner@outlook.com>
2020-01-28 23:33:14 +0800

    139. Word Break: Optimizes matching word and substring of text.

--- 139. Word Break/solution1.h
+++ 139. Word Break/solution1.h
@@ -5,18 +5,28 @@ class Solution {
 public:
     map<int, bool> dp;
     
-    bool BreakOneWord(string s, vector<string> & word_dict) {
-        if (s.size() == 0) return true;
-        else if (dp.count(s.size())) return dp[s.size()];
+    bool Match(string & s, int begin, string & w) {
+        if (s.size() < w.size()) return false;
         
-        for (auto & w: word_dict) {
-            if (s.find(w) == 0 && BreakOneWord(s.substr(w.size(), s.size() - w.size()), word_dict)) return true;
+        for (int i = 0; i < w.size(); ++i) {
+            if (s[begin + i] != w[i]) return false;
         }
         
-        return dp[s.size()] = false;
+        return true;
+    }
+    
+    bool BreakOneWord(string & s, int begin, vector<string> & word_dict) {
+        if (begin == s.size()) return true;
+        else if (dp.count(begin)) return dp[begin];
+        
+        for (auto & w: word_dict) {
+            if (Match(s, begin, w) && BreakOneWord(s, begin + w.size(), word_dict)) return true;
+        }
+        
+        return dp[begin] = false;
     }
     
     bool wordBreak(string s, vector<string>& wordDict) {
-        return BreakOneWord(s, wordDict);
+        return BreakOneWord(s, 0, wordDict);
     }
 };
