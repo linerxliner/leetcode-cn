3c039e178428426fa5158ad4554e002e6d908ec3
linerxliner <linerxliner@outlook.com>
2020-02-02 08:48:07 +0800

    295. Find Median from Data Stream: Two heaps.

---
+++ 295. Find Median from Data Stream/solution1.h
@@ -0,0 +1,35 @@
+// Two heaps.
+// Time: O(logn), Space: O(n)
+
+class MedianFinder {
+public:
+    priority_queue<int> lo;
+    priority_queue<int, vector<int>, greater<int>> hi;
+    
+    /** initialize your data structure here. */
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        lo.push(num);
+        hi.push(lo.top());
+        lo.pop();
+        
+        if (lo.size() < hi.size()) {
+            lo.push(hi.top());
+            hi.pop();
+        }
+    }
+    
+    double findMedian() {
+        return lo.size() > hi.size() ? lo.top() : (lo.top() + hi.top()) * 0.5;
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
