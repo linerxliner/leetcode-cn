b37510168ecf9b2de0ad91fcdc27285c2a20df76
linerxliner <linerxliner@outlook.com>
2020-02-11 20:05:56 +0800

    279. Perfect Squares: Lagrange's four-square theorem.

---
+++ 279. Perfect Squares/solution4.h
@@ -0,0 +1,16 @@
+// Lagrange's four-square theorem.
+// Time: O(n), Space: O(1)
+
+class Solution {
+public:
+    int numSquares(int n) {
+        while ((n & 3) == 0) n >>= 2;
+        if(int(sqrt(n)) * int(sqrt(n)) == n) return 1;
+        if ((n & 7) == 7) return 4;
+        for (int i = 1; i <= int(sqrt(n)); ++i) {
+            int r = n - i * i;
+            if (int(sqrt(r)) * int(sqrt(r)) == r) return 2;
+        }
+        return 3;
+    }
+};
