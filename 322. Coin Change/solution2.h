f01665fd9c080e15ae5a714ccb8a46831dde5bd7
linerxliner <linerxliner@outlook.com>
2020-02-12 10:22:22 +0800

    322. Coin Change: DFS.

---
+++ 322. Coin Change/solution2.h
@@ -0,0 +1,32 @@
+// DFS.
+// Space: O(n)
+
+class Solution {
+public:
+    int min_depth = INT_MAX;
+    
+    void DFS(vector<int>& coins, int amount, int begin, int depth) {
+        if (amount == 0) {
+            min_depth = min(min_depth, depth);
+            return;
+        } else if (amount < 0) return;
+        
+        if (amount % coins[begin] == 0) {
+            min_depth = min(min_depth, depth + amount / coins[begin]);
+            return;
+        }
+        
+        if (depth + (amount - 1) / coins[begin] + 1 >= min_depth) return;
+        
+        for (int i = begin; i < coins.size(); ++i) DFS(coins, amount - coins[i], i, depth + 1);
+    }
+    
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -1);
+        
+        sort(coins.begin(), coins.end(), greater<int>());
+        DFS(coins, amount, 0, 0);
+        
+        return min_depth == INT_MAX ? -1 : min_depth;
+    }
+};
