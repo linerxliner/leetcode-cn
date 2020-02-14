2328a87cd45b53b3c67531b48493f2e0a3908e8a
linerxliner <linerxliner@outlook.com>
2020-02-14 10:58:28 +0800

    190. Reverse Bits: Tricky way.

--- 190. Reverse Bits/solution1.h
+++ 190. Reverse Bits/solution1.h
@@ -1,10 +1,12 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        uint32_t m = 0;
+        n = ((n & 0xAAAAAAAA) >> 1) | ((n & 0x55555555) << 1);
+        n = ((n & 0xCCCCCCCC) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xF0F0F0F0) >> 4) | ((n & 0x0F0F0F0F) << 4);
+        n = ((n & 0xFF00FF00) >> 8) | ((n & 0x00FF00FF) << 8);
+        n = ((n & 0xFFFF0000) >> 16) | ((n & 0x0000FFFF) << 16);
         
-        for (int i = 31; i >= 0 && n > 0; --i, n >>= 1) m |= (n & 1) << i;
-        
-        return m;
+        return n;
     }
 };
