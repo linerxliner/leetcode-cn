855b2d404fa0d33fc905fd583eb5e23a04b9fe13
linerxliner <linerxliner@outlook.com>
2020-02-14 10:43:46 +0800

    190. Reverse Bits

---
+++ 190. Reverse Bits/solution1.h
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t m = 0;
+        
+        for (int i = 31; i >= 0 && n > 0; --i, n >>= 1) m |= (n & 1) << i;
+        
+        return m;
+    }
+};
