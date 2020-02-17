d13bba9ab58d139b9e57d53503ae2f07faff94b1
linerxliner <linerxliner@outlook.com>
2020-02-17 13:17:56 +0800

    344. Reverse String

---
+++ 344. Reverse String/Python/solution1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s_len = len(s)
+        for i in range(s_len // 2):
+            s[i], s[s_len - i - 1] = s[s_len - i - 1], s[i]
