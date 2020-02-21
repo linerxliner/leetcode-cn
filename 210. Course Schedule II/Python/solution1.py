cd3004d2844a86d33ce6c2afad789f2322d73f1a
linerxliner <linerxliner@outlook.com>
2020-02-21 15:26:12 +0800

    210. Course Schedule II

---
+++ 210. Course Schedule II/Python/solution1.py
@@ -0,0 +1,42 @@
+// DFS.
+// Time: O(v + e); Space: O(v)
+
+class Solution:
+    UNDISCOVERED = 0
+    DISCOVERED = 1
+    VISITED = 2
+    
+    def build_graph(self, course_num, prerequisites):
+        self.adj = [[] for _ in range(course_num)]
+        
+        for p in prerequisites:
+            self.adj[p[1]].append(p[0])
+        
+        self.state = [self.UNDISCOVERED] * course_num
+    
+    def DFS(self, u):
+        self.state[u] = self.DISCOVERED
+        
+        for v in self.adj[u]:
+            if self.state[v] == self.UNDISCOVERED:
+                if not self.DFS(v):
+                    return False
+            elif self.state[v] == self.DISCOVERED:
+                return False
+            
+        self.state[u] = self.VISITED
+        self.result.append(u)
+        
+        return True
+    
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        self.result = []
+        self.build_graph(numCourses, prerequisites)
+        
+        for u in range(numCourses):
+            if self.state[u] == self.UNDISCOVERED and not self.DFS(u):
+                return []
+        
+        return reversed(self.result)
+
+
