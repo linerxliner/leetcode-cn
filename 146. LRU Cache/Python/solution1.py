cf6c56e6d1bbd12723cac871263eda3c3f2f8aed
linerxliner <linerxliner@outlook.com>
2020-03-03 19:21:43 +0800

    146. LRU Cache

---
+++ 146. LRU Cache/Python/solution1.py
@@ -0,0 +1,30 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self._capacity = capacity
+        self._data = collections.OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key in self._data:
+            value = self._data[key]
+            self._data.pop(key)
+            self._data[key] = value
+        else:
+            value = -1
+        
+        return value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self._data:
+            self._data.pop(key)
+        else:
+            if len(self._data) >= self._capacity:
+                self._data.popitem(last=False)
+        self._data[key] = value
+            
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
