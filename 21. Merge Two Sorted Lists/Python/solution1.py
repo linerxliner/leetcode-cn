93ead5b4bd8c49517b17d491629ad74f1a0737df
linerxliner <linerxliner@outlook.com>
2020-03-11 14:22:44 +0800

    21. Merge Two Sorted Lists

---
+++ 21. Merge Two Sorted Lists/Python/solution1.py
@@ -0,0 +1,30 @@
+# Complexity
+#   Time: O(n)
+#   Space: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        l3 = ListNode(0)
+        curr = l3
+
+        while l1 and l2:
+            if l1.val <= l2.val:
+                curr.next = l1
+                l1 = l1.next
+            else:
+                curr.next = l2
+                l2 = l2.next
+            curr = curr.next
+
+        if l1:
+            curr.next = l1
+        elif l2:
+            curr.next = l2
+
+        return l3.next
