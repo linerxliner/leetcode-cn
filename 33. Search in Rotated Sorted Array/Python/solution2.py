fdb2feb5c69228b3c6a9e6c3b799b78356b26fd3
linerxliner <linerxliner@outlook.com>
2020-03-12 09:55:55 +0800

    33. Search in Rotated Sorted Array

--- 33. Search in Rotated Sorted Array/Python/solution2.py
+++ 33. Search in Rotated Sorted Array/Python/solution2.py
@@ -6,27 +6,23 @@
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 0:
+            return -1
+
         lo, hi = 0, len(nums) - 1
 
-        while lo <= hi:
+        while lo < hi:
             mi = (lo + hi) // 2
 
-            if nums[lo] < nums[mi]:
+            if nums[lo] <= nums[mi]:
                 if nums[lo] <= target <= nums[mi]:
                     hi = mi
                 else:
                     lo = mi + 1
-            elif nums[lo] > nums[mi]:
+            else:
                 if nums[mi] <= target <= nums[hi]:
                     lo = mi
                 else:
                     hi = mi - 1
-            else:
-                if nums[lo] == target:
-                    return lo
-                elif nums[hi] == target:
-                    return hi
-                else:
-                    return -1
 
-        return -1
+        return lo if nums[lo] == target else -1
