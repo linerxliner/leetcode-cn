9791f57e4615f9a0f5dacc59fd06285092b5026a
linerxliner <linerxliner@outlook.com>
2020-03-16 16:40:34 +0800

    22. Generate Parentheses

---
+++ 22. Generate Parentheses/C++/solution1.h
@@ -0,0 +1,37 @@
+/* Complexity
+ *   Time: O(4 ^ n / (n * sqrt(n)))
+ *   Space: O(4 ^ n / (n * sqrt(n)))
+ * Description
+ *   DFS with backtracking.
+ */
+
+class Solution {
+public:
+    vector<string> result;
+    string answer;
+    
+    void RGenerate(int unmatched, int unused) {
+        if (unmatched == 0 && unused == 0) {
+            result.push_back(answer);
+            return;
+        }
+        
+        if (unused > 0) {
+            answer.push_back('(');
+            RGenerate(unmatched + 1, unused - 1);
+            answer.pop_back();
+        }
+        
+        if (unmatched > 0) {
+            answer.push_back(')');
+            RGenerate(unmatched - 1, unused);
+            answer.pop_back();
+        }
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        RGenerate(0, n);
+        
+        return result;
+    }
+};
---
+++ 22. Generate Parentheses/C++/solution2.h
@@ -0,0 +1,28 @@
+/* Complexity
+ *   Time: O(4 ^ n /(n ^ (2 / 3)))
+ *   Space: O(4 ^ n /(n ^ (2 / 3)))
+ * Description
+ *   DP.
+ */
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        map<int, vector<string>> dp;
+
+        dp[0] = vector<string>({""});
+
+        for (int k = 1; k <= n; ++k) {
+            dp[k] = vector<string>();
+            for (int i = 0; i < k; ++i) {
+                for (const auto& s1: dp[i]) {
+                    for (const auto& s2: dp[k - i - 1]) {
+                        dp[k].push_back("(" + s1 + ")" + s2);
+                    }
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
