4c816fa91b910b822459464cc95a372c6dca4e3b
linerxliner <linerxliner@outlook.com>
2020-03-22 16:05:40 +0800

    907. Sum of Subarray Minimums

--- 907. Sum of Subarray Minimums/C++/solution1.h
+++ 907. Sum of Subarray Minimums/C++/solution1.h
@@ -9,19 +9,20 @@ public:
 
     int sumSubarrayMins(vector<int>& A) {
         int sum_subarray_mins = 0;
-        deque<int> dq;
+        stack<int> s;
         vector<int> new_A(A.size() + 2);
 
         new_A[0] = new_A[new_A.size() - 1] = INT_MIN;
         for (int i = 1; i < new_A.size() - 1; ++i) new_A[i] = A[i - 1];
 
         for (int i = 0; i < new_A.size(); ++i) {
-            while (!dq.empty() && new_A[dq.back()] > new_A[i]) {
-                sum_subarray_mins += new_A[dq.back()] * (dq.back() - *(prev(dq.end(), 2))) * (i - dq.back());
+            while (!s.empty() && new_A[s.top()] > new_A[i]) {
+                int t = s.top(); s.pop();
+
+                sum_subarray_mins += new_A[t] * (t - s.top()) * (i - t);
                 sum_subarray_mins %= kMod;
-                dq.pop_back();
             }
-            dq.push_back(i);
+            s.push(i);
         }
 
         return sum_subarray_mins;
